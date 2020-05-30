453cd846280c48a41a97dd0e30b98fcaceae150a
tjr226 <tjr226@gmail.com>
2020-05-30 12:44:44 -0400

    clues view instructions complete

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Timothy Rooney on 5/30/20.
+//  Copyright © 2020 Timothy Rooney. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
