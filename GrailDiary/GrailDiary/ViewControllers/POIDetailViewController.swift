bfe6e856bcb2fd4b39a9552be7284dc63365da6f
tjr226 <tjr226@gmail.com>
2020-06-02 13:19:25 -0400

    all instructions for assignment complete, but now the nav bar is missing

--- GrailDiary/GrailDiary/ViewControllers/POIDetailViewController.swift
+++ GrailDiary/GrailDiary/ViewControllers/POIDetailViewController.swift
@@ -8,23 +8,33 @@
 
 import UIKit
 
+
 class POIDetailViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var cluesTextView: UITextView!
+    
+    var poi: POI?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func updateViews() {
+        guard let poi = poi else { return }
+        
+        locationLabel.text = poi.location
+        countryLabel.text = poi.country
+        
+        var clueText = ""
+        for clue in poi.clues {
+            clueText += "- \(clue)\n"
+        }
+        
+        cluesTextView.text = clueText
     }
-    */
-
+    
 }
